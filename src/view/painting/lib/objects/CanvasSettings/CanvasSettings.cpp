4a1792c9633211b4587c2bcc02a061891c14819e
willianbrito.contato@gmail.com <Willian Silva>
2026-09-05 09:35:55 -0300

    🐛 Correção Pontono Espaço

--- src/view/painting/lib/objects/CanvasSettings/CanvasSettings.cpp
+++ src/view/painting/lib/objects/CanvasSettings/CanvasSettings.cpp
@@ -19,20 +19,20 @@ Point CanvasSettings::cursorToCanvas(int x, int y){
 Point CanvasSettings::canvasToWorld(int x, int y){
     Point p;
     p.x = (tilingContext.isTilingX) ?
-        (int)std::floor((x - canvasTransform.pan.x) / (canvasTransform.scale / TilingContext::N_TILE_X)) :
-        (int)std::floor((x - canvasTransform.pan.x) / (canvasTransform.scale));
+        (int)std::floor((x + canvasTransform.pan.x) * (canvasTransform.scale / TilingContext::N_TILE_X)) :
+        (int)std::floor((x + canvasTransform.pan.x) * (canvasTransform.scale));
     p.y = (tilingContext.isTilingY) ? 
-        (int)std::floor((y - canvasTransform.pan.y) / (canvasTransform.scale / TilingContext::N_TILE_Y)) :
-        (int)std::floor((y - canvasTransform.pan.y) / (canvasTransform.scale));
+        (int)std::floor((y + canvasTransform.pan.y) * (canvasTransform.scale / TilingContext::N_TILE_Y)) :
+        (int)std::floor((y + canvasTransform.pan.y) * (canvasTransform.scale));
     return p;
 }
 PointF CanvasSettings::canvasToWorld(float x, float y){
     PointF p;
     p.x = (tilingContext.isTilingX) ?
-        (x - canvasTransform.pan.x) / (canvasTransform.scale / TilingContext::N_TILE_X) :
-        (x - canvasTransform.pan.x) / (canvasTransform.scale);
+        (x + canvasTransform.pan.x) * (canvasTransform.scale / TilingContext::N_TILE_X) :
+        (x + canvasTransform.pan.x) * (canvasTransform.scale);
     p.y = (tilingContext.isTilingY) ? 
-        (y - canvasTransform.pan.y) / (canvasTransform.scale / TilingContext::N_TILE_Y) :
-        (y - canvasTransform.pan.y) / (canvasTransform.scale);
+        (y + canvasTransform.pan.y) * (canvasTransform.scale / TilingContext::N_TILE_Y) :
+        (y + canvasTransform.pan.y) * (canvasTransform.scale);
     return p;
 }
