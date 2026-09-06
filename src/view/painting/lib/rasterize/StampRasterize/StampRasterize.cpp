e30e7abea8b7b3674a40fbf0ada798f371373b19
willianbrito.contato@gmail.com <Willian Silva>
2026-09-06 14:39:22 -0300

    ⚰️ Remocao comentarios

--- src/view/painting/lib/rasterize/StampRasterize/StampRasterize.cpp
+++ src/view/painting/lib/rasterize/StampRasterize/StampRasterize.cpp
@@ -6,16 +6,10 @@ StampRasterize::StampRasterize(const PointF& center, const Point& sizePattern, c
     
     _transformation.transform(bounding, sizePattern, center);
 
-    const PointF* d = transformation.getDelta();
-    const float rad = transformation.getRad();
-    const PointF* _scale = transformation.getScale();
-    
-    printf("(%u,%u) - ", sizePattern.x, sizePattern.y);
     bounding.start.x = bounding.start.x < 0 ? 0 : bounding.start.x;
     bounding.start.y = bounding.start.y < 0 ? 0 : bounding.start.y;
     bounding.end.x = bounding.end.x >= crop.x ? crop.x : bounding.end.x;
     bounding.end.y = bounding.end.y >= crop.y ? crop.y : bounding.end.y;
-    printf("(%u,%u)\n", bounding.getWidth(), bounding.getHeight());
     
     _current =  bounding.start;
     _hasNext = bounding.start.x < bounding.end.x && bounding.start.y < bounding.end.y;
