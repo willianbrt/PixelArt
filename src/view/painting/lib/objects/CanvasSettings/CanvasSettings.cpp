4bd1c8a23fcada7052facbe4aa519e7bca53bc52
willianbrito.contato@gmail.com <Willian Silva>
2026-09-06 10:03:31 -0300

    🚧 correção canvas to world

--- src/view/painting/lib/objects/CanvasSettings/CanvasSettings.cpp
+++ src/view/painting/lib/objects/CanvasSettings/CanvasSettings.cpp
@@ -6,34 +6,19 @@ CanvasSettings::CanvasSettings(){
 
 Point CanvasSettings::cursorToCanvas(int x, int y){
     Point p;
-    p.x = (tilingContext.isTilingX) ?
-        (int)std::floor((x - canvasTransform.pan.x) / (canvasTransform.scale / TilingContext::N_TILE_X)) :
-        (int)std::floor((x - canvasTransform.pan.x) / (canvasTransform.scale));
-    p.y = (tilingContext.isTilingY) ? 
-        (int)std::floor((y - canvasTransform.pan.y) / (canvasTransform.scale / TilingContext::N_TILE_Y)) :
-        (int)std::floor((y - canvasTransform.pan.y) / (canvasTransform.scale));
-
+    p.x = (int)std::floor((x - canvasTransform.pan.x) / (canvasTransform.scale / (tilingContext.isTilingX ? TilingContext::N_TILE_X : 1)));
+    p.y = (int)std::floor((y - canvasTransform.pan.y) / (canvasTransform.scale / (tilingContext.isTilingY ? TilingContext::N_TILE_Y : 1)));
     return p;
 }
 
 Point CanvasSettings::canvasToWorld(int x, int y){
-    Point p;
-    p.x = (tilingContext.isTilingX) ?
-        (int)std::floor((x + canvasTransform.pan.x) * (canvasTransform.scale / TilingContext::N_TILE_X)) :
-        (int)std::floor((x + canvasTransform.pan.x) * (canvasTransform.scale));
-    p.y = (tilingContext.isTilingY) ? 
-        (int)std::floor((y + canvasTransform.pan.y) * (canvasTransform.scale / TilingContext::N_TILE_Y)) :
-        (int)std::floor((y + canvasTransform.pan.y) * (canvasTransform.scale));
-    return p;
+    PointF p = canvasToWorld((float) x, (float) y);
+    return { (int)p.x, (int)p.y };
 }
 PointF CanvasSettings::canvasToWorld(float x, float y){
     PointF p;
-    p.x = (tilingContext.isTilingX) ?
-        (x + canvasTransform.pan.x) * (canvasTransform.scale / TilingContext::N_TILE_X) :
-        (x + canvasTransform.pan.x) * (canvasTransform.scale);
-    p.y = (tilingContext.isTilingY) ? 
-        (y + canvasTransform.pan.y) * (canvasTransform.scale / TilingContext::N_TILE_Y) :
-        (y + canvasTransform.pan.y) * (canvasTransform.scale);
+    p.x = canvasTransform.pan.x + x * (canvasTransform.scale / (tilingContext.isTilingX ? TilingContext::N_TILE_X : 1));
+    p.y = canvasTransform.pan.y + y * (canvasTransform.scale / (tilingContext.isTilingY ? TilingContext::N_TILE_Y : 1));
     return p;
 }
 
